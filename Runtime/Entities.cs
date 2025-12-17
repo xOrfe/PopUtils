9a1e05be077aa80638842f9271247715e672402b
xOrfe <batuhanozankaplan@protonmail.com>
2025-12-17 07:38:56 +0300

    Updates related entitiy transform system

---
+++ Runtime/Entities.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+namespace XO.PopUtils
+{
+    public static partial class Pop
+    {
+        public static void SetParent(this Entity entity, EntityManager em, EntityCommandBuffer ecb, Entity parent)
+        {
+            if (em.HasComponent<Parent>(entity))
+            {
+                ecb.SetComponent(entity, new Parent
+                {
+                    Value = parent
+                });
+            }
+            else
+            {
+                ecb.AddComponent(entity, new Parent
+                {
+                    Value = parent
+                });
+            }
+
+            if (!em.HasComponent<LocalTransform>(entity))
+            {
+                ecb.AddComponent(entity, LocalTransform.Identity);
+            }
+        }
+    }
+}
---
+++ Runtime/Transform.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.CompilerServices;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace XO.PopUtils
+{
+    public static partial class Pop
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float4x4 ToWorldSpace(float4x4 parent, float4x4 local)
+        {
+            return math.mul(parent, local);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static LocalToWorld ToWorldSpace(LocalToWorld parent, LocalToWorld local)
+        {
+            return new LocalToWorld
+            {
+                Value = math.mul(parent.Value, local.Value)
+            };
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float4x4 GetRelativeTransform(float4x4 reference, float4x4 target)
+        {
+            return math.mul(math.inverse(reference), target);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static LocalToWorld GetRelativeTransform(LocalToWorld reference, LocalToWorld target)
+        {
+            return new LocalToWorld
+            {
+                Value = math.mul(math.inverse(reference.Value), target.Value)
+            };
+        }
+    }
+}
