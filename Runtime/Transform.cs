c7110a28d755fd5fab8009bd2533572d0bdef7d5
xOrfe <batuhanozankaplan@protonmail.com>
2025-12-29 20:38:39 +0300

    Entity Transform

--- Runtime/Transform.cs
+++ Runtime/Transform.cs
@@ -7,6 +7,19 @@ namespace XO.PopUtils
 {
     public static partial class Pop
     {
+        
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static LocalTransform NewLocalTransform()
+        {
+            return new LocalTransform()
+            {
+                Position = float3.zero,
+                Rotation = quaternion.identity,
+                Scale = 1
+            };
+        }
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float4x4 ToWorldSpace(float4x4 parent, float4x4 local)
         {
@@ -36,5 +49,21 @@ namespace XO.PopUtils
                 Value = math.mul(math.inverse(reference.Value), target.Value)
             };
         }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float4x4 LocalizedTransform(float4x4 reference, float4x4 target)
+        {
+            return math.mul(reference, target);
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static LocalToWorld LocalizedTransform(LocalToWorld reference, LocalToWorld target)
+        {
+            return new LocalToWorld
+            {
+                Value = math.mul(reference.Value, target.Value)
+            };
+        }
+        
     }
 }
